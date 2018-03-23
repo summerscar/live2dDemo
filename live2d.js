2ebde1fe93fe2b8f55d2a7cbd12e2ad45e7a3ac1
shenyu <shen.yus@163.com>
2018-03-23 16:37:25 +0800

    update

---
+++ live2d.js
@@ -0,0 +1,4 @@
+var link =  document.createElement("link")
+link.setAttribute('href', 'http://summerscar.me/Live2D/live2d/css/live2d.css')
+link.setAttribute('rel', 'stylesheet')
+document.head.appendChild(link)
